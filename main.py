174266569f11538c0c77b2e98734bc2b59c1cff8
erick2990 <e30rick@gmail.com>
2025-07-30 10:54:50 -0600

    Arreglando funciones

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def calcular_MCD(num_1, num_2): #funcion recursiva para calcular el mcd de dos n
     if res==0:
         return 0
     else:
-        return calc_numeros(num1, num2)
+        return calc_numeros(1,3)
 
 
 def cadena_rep(palabra, cantidad): #Funcion para repetir una palabra varias veces
@@ -16,22 +16,23 @@ def cadena_rep(palabra, cantidad): #Funcion para repetir una palabra varias vece
         return  cadena_rep(palabra, cantidad)
 
 
-
 def contar_cadena(palabra, letra): #funcion para contar cuantas veces se repite una letra en una palabra
     palabra_nueva = palabra.lower()
     letra_nueva = letra.lower()
-    if palabra_nueva == "":
-        return
-    elif palabra_nueva[0] ==letra_nueva:
-        existencia =+1
-        print(existencia)
-        return contar_cadena(palabra[1:], letra)
+
+    if palabra == " ":
+        return 0
+    else:
+        if letra_nueva == palabra_nueva[0]:
+             return 1 + contar_cadena(palabra[1:], letra)
+        else:
+            return contar_cadena(palabra[1:] ,  letra)
+
+
 
 def convertir_decimalB(numero): #funcion para convertir decimal a binario
     if numero==0:
         return
-    else:
-        residuo =
 
 
 
@@ -41,6 +42,7 @@ def calc_numeros(n): #funcion recursiva para calcular numeros
         return 0
     else:
         return cantidad + calc_numeros(cantidad[1:])
+
 
 
 fin_menu = True
@@ -60,8 +62,10 @@ while fin_menu:
             case 3:
                 cadena = input('Ingres el texto: ')
                 letra = input('Que letra desea saber cuantas veces se repite')
-                contar_cadena(cadena, letra)
-            #case 4:
+                print(contar_cadena(cadena, letra))
+            case 4:
+                decimal = input('Ingrese el numero en decial que desea convertir a binario: ')
+                convertir_decimalB(decimal)
             case 5:
                 digitos = int(input('Ingrese el numero del que desea saber cuantos digitos tiene'))
                 print(calc_numeros(digitos))
